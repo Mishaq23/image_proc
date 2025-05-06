339f85ea841395b56afc0138e85f202d70c993b3
Mikhail Myasnikov <87562491+Mishaq23@users.noreply.github.com>
2025-05-06 14:24:49 +0300

    Update apply_filters.h

--- apply_filters.h
+++ apply_filters.h
@@ -8,24 +8,7 @@
 #include <stdexcept>
 #include <memory>
 
-#include "image.h"
-#include "filter.h"
-#include "cmd_line_args_parser.h"
-#include "filters/blur.h"
-#include "filters/crop.h"
-#include "filters/edge.h"
-#include "filters/gs.h"
-#include "filters/neg.h"
-#include "filters/sepia.h"
-#include "filters/sharp.h"
-
-std::unique_ptr<Blur> MakeBlur(const FilterDescriptor* fd);
-std::unique_ptr<Crop> MakeCrop(const FilterDescriptor* fd);
-std::unique_ptr<Edge> MakeEdge(const FilterDescriptor* fd);
-std::unique_ptr<Gs> MakeGs(const FilterDescriptor* fd);
-std::unique_ptr<Neg> MakeNeg(const FilterDescriptor* fd);
-std::unique_ptr<Sepia> MakeSepia(const FilterDescriptor* fd);
-std::unique_ptr<Sharp> MakeSharp(const FilterDescriptor* fd);
+#include "filter_factory.h"
 
 class ApplyFilters {
 public:
